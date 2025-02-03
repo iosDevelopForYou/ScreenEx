709a6d931e4fde7fd58ee09f29f486f4eb9624ce
iosDevelopForYou <iosdevelopforyou@gmail.com>
2025-02-03 19:01:19 +0400

    Initial Commit

---
+++ ScreenEx/ScreenExApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenExApp.swift
+//  ScreenEx
+//
+//  Created by Marat Guseynov on 03.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ScreenExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
